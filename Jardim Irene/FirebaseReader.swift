0a8fa7e0bf329f6dda4d39574e970ec959850385
Brunosccp <rocca_bruno@hotmail.com>
2018-07-31 20:35:39 -0300

    informações da partida indo pra viewController

--- Jardim Irene/FirebaseReader.swift
+++ Jardim Irene/FirebaseReader.swift
@@ -12,9 +12,9 @@ import Firebase
 class FirebaseReader{
     let ref = Database.database().reference()
     
-    func getMatchesInfo(completion: @escaping (_ matchday: Int, _ count: Int) -> Void) {
+    func getMatchesInfo(completion: @escaping (_ matchday: Int, _ count: [[String : Any]]) -> Void) {
         //obtendo referencia de matches
-        let matchesList: [[String : Any]]
+        var matchesList: [[String : Any]] = []
         
         ref.child("BrazilSerieA").child("matches").observeSingleEvent(of: .value, with: {(snapshot) in
             let value = snapshot.value as? NSDictionary
@@ -24,20 +24,21 @@ class FirebaseReader{
             for key in value!.allKeys{
                 self.ref.child("BrazilSerieA").child("matches").child("\(key)").observeSingleEvent(of: .value, with: {(snapshot2) in
                     let value2 = snapshot2.value as? NSDictionary
-                    print(key)
+                    
                     //testando se é partida (por causa do currentMatchday) e se a partida é da rodada atual
                     if(value2?["homeTeamID"] != nil && value2?["matchday"] as? Int == matchday){
-                        print("time da casa id: ", value2!["homeWinOdd"]!)
+                        //convertendo NSDictionary para [String : Any] e assim, jogando na lista
+                        let mom = value2! as! [String : Any]
+                        matchesList.append(mom)
+                        
+                    }   //chegou no matchday, vulgo ultima informação de matches, então retorna
+                    else{
+                        //print(matchesList)
+                        completion(matchday!, matchesList)
                     }
                     
                 })
             }
-            
-            
-            
-            completion(matchday!, value!.count)
-            
         })
     }
-    
 }
